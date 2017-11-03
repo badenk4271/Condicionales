fae297b9ce4e0ac2a0b069fca1b7d00eecb94881
badenk4271 <ayavarav@gmail.com>
2017-11-03 19:24:59 -0300

    Se  utizan if y else para crear el ejercicio

--- src/condicionales/Calificacion2.java
+++ src/condicionales/Calificacion2.java
@@ -4,11 +4,57 @@
  * and open the template in the editor.
  */
 package condicionales;
-
+import javax.swing.JOptionPane;
 /**
  *
  * @author LAB01
  */
-public class Calificacion2 {
+public class Calificacion2 
+{
+ 
+  public static void main(String[] args)  
+  {
+  float nota = Float.parseFloat(JOptionPane.showInputDialog("Ingrese nota"));
+  
+  
+
+  
+   
+   
+   if(nota>=0 && nota<4){
+ JOptionPane.showMessageDialog(null,"Insuficiente");
+   }
+ else
+ if(nota>=4 && nota<=5){
+ JOptionPane.showMessageDialog(null,"Suficiente");
+ }
+ else
+ if(nota>=6) {
+ JOptionPane.showMessageDialog(null,"Bien");
+ }
+ else
+ if (nota==7)
+ {
+ JOptionPane.showMessageDialog(null,"Notable");
+  
+   
+  }
+  }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
 }
