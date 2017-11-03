be3a2baae1392f142ffa81fd22b3ac3e0fa980ae
badenk4271 <ayavarav@gmail.com>
2017-11-02 21:15:57 -0300

    Se usaron condicionales if y else para mostrar un promedio y ver si aprueba o reprueba segun prommedio

--- src/condicionales/Calificaciones.java
+++ src/condicionales/Calificaciones.java
@@ -4,11 +4,39 @@
  * and open the template in the editor.
  */
 package condicionales;
-
+import javax.swing.JOptionPane;
 /**
  *
  * @author LAB01
  */
 public class Calificaciones {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+     float nota1, nota2, nota3, promedio;
+    nota1 = Float.parseFloat(JOptionPane.showInputDialog("Ingrese nota"));
+    nota2 = Float.parseFloat(JOptionPane.showInputDialog("Ingrese nota"));
+    nota3 = Float.parseFloat(JOptionPane.showInputDialog("Ingrese nota"));
+    
+    promedio = (nota1 + nota2 + nota3)/3;
+    JOptionPane.showMessageDialog(null,"su promedio es: "+promedio);
+   // Si la condicion se cumple se ejecuta este codigo
+    if (promedio>=4) //Aqui va una condicion 
+    { 
+      
+    JOptionPane.showMessageDialog(null,"Aprobo");
+      
+    }
+    
+    else//Si no se cumple la condicion se ejecuta esto
+        JOptionPane.showMessageDialog(null,"Reprobo");
+            
     
 }
+    } 
+
+
+
